786841e5095322e6a3566b0314796d9dd3f1c317
reiinakano <reii_nakano@yahoo.com>
2016-04-16 03:14:28 +0800

    commit experiment

--- snifferv0.py
+++ snifferv0.py
@@ -2,9 +2,9 @@ import pyshark, time
 
 if __name__ == '__main__':
     print("Start capture")
-    capture = pyshark.LiveCapture(interface="wlan1mon")
+    capture = pyshark.LiveCapture(interface="wlan0")
     i = 1
-    for packet in capture.sniff_continuously(packet_count=10):
+    for packet in capture.sniff_continuously(packet_count=200):
         print '----------------Just arrived: Packet number', i
         print packet
         i+=1
