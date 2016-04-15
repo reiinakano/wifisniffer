59e860f76b197c5827a2b955a551ddfac130193a
reiinakano <reii_nakano@yahoo.com>
2016-04-16 00:59:53 +0800

    Initial commit

---
+++ snifferv0.py
@@ -0,0 +1,10 @@
+import pyshark, time
+
+if __name__ == '__main__':
+    print("Start capture")
+    capture = pyshark.LiveCapture(interface="wlan1mon")
+    i = 1
+    for packet in capture.sniff_continuously(packet_count=10):
+        print '----------------Just arrived: Packet number', i
+        print packet
+        i+=1
