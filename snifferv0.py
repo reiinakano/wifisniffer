7839a78bc77a7a401c7a2ea332e2d7c5c3a56600
reiinakano <reii_nakano@yahoo.com>
2016-04-16 14:27:13 +0800

    commit experiment

--- snifferv0.py
+++ snifferv0.py
@@ -1,10 +1,14 @@
-import pyshark, time
+import pyshark, time, subprocess32
 
 if __name__ == '__main__':
-    print("Start capture")
-    capture = pyshark.LiveCapture(interface="wlan0")
-    i = 1
-    for packet in capture.sniff_continuously(packet_count=200):
-        print '----------------Just arrived: Packet number', i
-        print packet
-        i+=1
+    #print("Start capture")
+    #capture = pyshark.LiveCapture(interface="wlan0")
+    #i = 1
+    #for packet in capture.sniff_continuously(packet_count=200):
+    #    print '----------------Just arrived: Packet number', i
+    #    print packet
+    #   i+=1
+    proc = subprocess32.Popen(['d11decrypt/build/dot11decrypt', 'wlan0', 'wpa:thisismyssid:thisismypass'])
+    time.sleep(1)
+    print proc.poll()
+    print proc.returncode
