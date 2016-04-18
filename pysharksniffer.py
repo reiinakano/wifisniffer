d3743c60de465a2991ebe32e95901603d900d807
reiinakano <reii_nakano@yahoo.com>
2016-04-19 05:28:12 +0800

    commit experiment

--- pysharksniffer.py
+++ pysharksniffer.py
@@ -1,6 +1,8 @@
 import pyshark
+import threading
 
 
-class PysharkMainSniffer(): # This class starts the PyShark master sniffer that updates a list of communicating APs and a list of established communications
+class PysharkMainSniffer(threading.Thread): # This class starts the PyShark master sniffer that updates a list of communicating APs and a list of established communications
     def __init__(self, interface_string):
+        threading.Thread.__init__(self)
         self.cap = pyshark.LiveCapture(interface=interface_string)
