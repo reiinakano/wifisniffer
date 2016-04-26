fbd0ac3747b3c76a365ff22d6a9b7af1b8e7726a
reiinakano <reii_nakano@yahoo.com>
2016-04-27 02:17:59 +0800

    gui changes

--- gui.py
+++ gui.py
@@ -25,19 +25,39 @@ class MyGUI(Frame):
         self.AP_selected = Label(self.parent, text=0, textvariable=self.AP_selected_text)
         self.AP_selected.grid(padx=5, pady=5)
 
-        self.AP_listbox = Listbox(self.parent)
+        self.AP_listbox = Listbox(self.parent, width=30)
         for AP in self.APList:
             if AP.SSID is not None:
                 self.AP_listbox.insert(END, AP.SSID)
             else:
                 self.AP_listbox.insert(END, AP.MAC)
-        self.AP_listbox.grid(row=1, padx=5)
+        self.AP_listbox.grid(row=1, padx=5, rowspan=10)
         self.AP_listbox.bind("<<ListboxSelect>>", self.onSelectAP)
 
         self.AP_listbox_scrollbar = Scrollbar(self.parent, orient=VERTICAL)
         self.AP_listbox.config(yscrollcommand=self.AP_listbox_scrollbar.set)
         self.AP_listbox_scrollbar.config(command=self.AP_listbox.yview)
-        self.AP_listbox_scrollbar.grid(row=0, column=1, rowspan=2)
+        self.AP_listbox_scrollbar.grid(row=1, column=1, rowspan=10, sticky = 'ns')
+
+        self.AP_BSSID_text = StringVar()
+        self.AP_BSSID = Label(self.parent, text="BSSID: ", width=30, textvariable=self.AP_BSSID_text, anchor=W)
+        self.AP_BSSID.grid(row=4, column=2, padx=5, sticky=W)
+
+        self.AP_SSID_text = StringVar()
+        self.AP_SSID = Label(self.parent, text="SSID: ", width=30, textvariable=self.AP_SSID_text, anchor=W)
+        self.AP_SSID.grid(row=2, column=2, padx=5, sticky=W)
+
+        self.AP_channel_text = StringVar()
+        self.AP_channel = Label(self.parent, text="Channel: ", width=30, textvariable=self.AP_channel_text, anchor=W)
+        self.AP_channel.grid(row=6, column=2, padx=5, sticky=W)
+
+        self.AP_enc_text = StringVar()
+        self.AP_enc = Label(self.parent, text="Encryption: ", width=30, textvariable=self.AP_enc_text, anchor=W)
+        self.AP_enc.grid(row=2, column=3, padx=5, sticky=W)
+
+        self.AP_Pass_text = StringVar()
+        self.AP_Pass = Label(self.parent, text="Password Known? ", width=30, textvariable=self.AP_Pass_text, anchor=W)
+        self.AP_Pass.grid(row=4, column=3, padx=5, sticky=W)
 
     def onSelectAP(self, val):
         sender = val.widget
@@ -45,9 +65,27 @@ class MyGUI(Frame):
         value = sender.get(index)
         self.AP_selected_text.set("AP: " + value)
 
+        numIndex = index[0]
+        self.AP_BSSID_text.set("BSSID: " + self.APList[numIndex].MAC)
+        self.AP_channel_text.set("Channel: " + str(self.APList[numIndex].channel))
+
+        if self.APList[numIndex].SSID is not None:
+            self.AP_SSID_text.set("SSID: " + self.APList[numIndex].SSID)
+        else:
+            self.AP_SSID_text.set("SSID: hidden or not yet known")
+
+        if self.APList[numIndex].encryption is not None:
+            self.AP_enc_text.set("Encryption: " + self.APList[numIndex].encryption)
+        else:
+            self.AP_enc_text.set("Encryption: Not yet known")
+
+        if self.APList[numIndex].password is not None:
+            self.AP_Pass_text.set("Password is known")
+        else:
+            self.AP_Pass_text.set("Password unknown")
+
     def updateData(self): # This method refreshes all data
         current_index = self.AP_listbox.curselection()
-        print current_index
         self.AP_listbox.delete(0, END)
         for AP in self.APList:
             if AP.SSID is not None:
@@ -57,7 +95,7 @@ class MyGUI(Frame):
         try:
             self.AP_listbox.selection_set(current_index)
             self.AP_listbox.see(current_index)
-            self.AP_listbox.selection_anchor(current_index)
+            self.AP_listbox.activate(current_index)
         except Exception as e:
             print e
 
